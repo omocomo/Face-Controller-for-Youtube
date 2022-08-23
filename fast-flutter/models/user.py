2e0f6d68ae88e3cf3a8611df564cdabb718f5841
omocomo <omocomo@naver.com>
2022-08-23 17:12:30 +0900

    ignore db

--- fast-flutter/models/user.py
+++ fast-flutter/models/user.py
@@ -1,12 +1,12 @@
-from sqlalchemy import Table, Column
-from sqlalchemy.sql.sqltypes import Integer, String
-from config.db import meta, engine
+# from sqlalchemy import Table, Column
+# from sqlalchemy.sql.sqltypes import Integer, String
+# from config.db import meta, engine
 
-users = Table('users', meta,
-Column('id', Integer, primary_key=True),
-Column('name', String(255)),
-Column('email', String(255)),
-Column('password', String(255))
-)
+# users = Table('users', meta,
+# Column('id', Integer, primary_key=True),
+# Column('name', String(255)),
+# Column('email', String(255)),
+# Column('password', String(255))
+# )
 
-meta.create_all(engine)
+# meta.create_all(engine)
