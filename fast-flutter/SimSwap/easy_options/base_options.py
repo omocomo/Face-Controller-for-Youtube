d67c3519ee01ce33ca6e4ebb2e136f6fe8d6d575
omocomo <omocomo@naver.com>
2022-08-23 17:14:39 +0900

    move SimSwap

---
+++ fast-flutter/SimSwap/easy_options/base_options.py
@@ -0,0 +1,103 @@
+import easydict
+import os
+from util import util
+import torch
+
+class BaseOptions():
+    # def __init__(self):
+        # self.parser = argparse.ArgumentParser()
+        # self.initialized = False
+
+    def initialize(self):
+        # experiment specifics
+        args = easydict.EasyDict({
+            # experiment specifics
+            "name": 'people',
+            "gpu_ids": '0',
+            "checkpoints_dir": './checkpoints',
+            "model": 'pix2pixHD',
+            "norm": 'batch',
+            "use_dropout": True,
+            "data_type": 32,
+            "verbose": False,
+            "fp16": False,
+            "local_rank": 0,
+            "isTrain": True,
+            # input/output sizes 
+            "batchSize": 8,
+            "loadSize": 1024,
+            "fineSize": 512,
+            "label_nc": 0,
+            "input_nc": 3,
+            "output_nc": 3,
+            # for setting inputs
+            "dataroot": './datasets/cityscapes/',
+            "resize_or_crop": 'scale_width',
+            "serial_batches": True,
+            "no_flip": True,
+            "nThreads": 2,
+            "max_dataset_size": float("inf"),
+            # for displays
+            "display_winsize": 512,
+            "tf_log": True,
+            # for generator 
+            "netG": 'global',
+            "latent_size": 512,
+            "ngf": 64,
+            "n_downsample_global": 3,
+            "n_blocks_global": 6,
+            "n_blocks_local": 3,
+            "n_local_enhancers": 1,
+            "niter_fix_global": 0,
+            # for instance-wise features
+            "no_instance": True,
+            "instance_feat": True,
+            "label_feat": True,
+            "feat_num": 3,
+            "load_features": True,
+            "n_downsample_E": 4,
+            "nef": 16,
+            "n_clusters": 10,
+            "image_size": 224,
+            "norm_G": 'spectralspadesyncbatch3x3',
+            "semantic_nc": 3
+        })
+
+        return args
+
+    def parse(self, save=True):
+        # if not self.initialized:
+        #     self.initialize()
+        # self.opt = self.parser.parse_args()
+        # self.opt.isTrain = self.isTrain   # train or test
+
+        # str_ids = self.opt.gpu_ids.split(',')
+        # self.opt.gpu_ids = []
+        # for str_id in str_ids:
+        #     id = int(str_id)
+        #     if id >= 0:
+        #         self.opt.gpu_ids.append(id)
+        
+        # # set gpu ids
+        # if len(self.opt.gpu_ids) > 0:
+        #     torch.cuda.set_device(self.opt.gpu_ids[0])
+
+        # args = vars(self.opt)
+
+        print('------------ Options -------------')
+        for k, v in sorted(args.items()):
+            print('%s: %s' % (str(k), str(v)))
+        print('-------------- End ----------------')
+
+        # save to the disk
+        # if self.opt.isTrain:
+        #     expr_dir = os.path.join(self.opt.checkpoints_dir, self.opt.name)
+        #     util.mkdirs(expr_dir)
+        #     if save and not self.opt.continue_train:
+        #         file_name = os.path.join(expr_dir, 'opt.txt')
+        #         with open(file_name, 'wt') as opt_file:
+        #             opt_file.write('------------ Options -------------\n')
+        #             for k, v in sorted(args.items()):
+        #                 opt_file.write('%s: %s\n' % (str(k), str(v)))
+        #             opt_file.write('-------------- End ----------------\n')
+        return args
---
+++ fast-flutter/SimSwap/easy_options/test_options.py
@@ -0,0 +1,39 @@
+'''
+Author: Naiyuan liu
+Github: https://github.com/NNNNAI
+Date: 2021-11-23 17:03:58
+LastEditors: Naiyuan liu
+LastEditTime: 2021-11-23 17:08:08
+Description: 
+'''
+from .base_options import BaseOptions
+
+class TestOptions(BaseOptions):
+    def initialize(self):
+        args = BaseOptions.initialize(self)
+        args["ntest"] = float("inf")
+        args["results_dir"] = './results/'
+        args["aspect_ratio"] = 1.0
+        args["phase"] = 'test'
+        args["which_epoch"] = 'latest'
+        args["how_many"] = 50
+        args["cluster_path"] = 'features_clustered_010.npy'
+        args["use_encoded_image"] = True
+        args["export_onnx"] = ''
+        args["engine"] = ''
+        args["onnx"] = ''
+        args["Arc_path"] = 'models/BEST_checkpoint.tar'
+        args["pic_a_path"] = './crop_224/gdg.jpg'
+        args["pic_b_path"] = './crop_224/zrf.jpg'
+        args["pic_specific_path"] = './crop_224/zrf.jpg'
+        args["multisepcific_dir"] = './demo_file/multispecific'
+        args["video_path"] = './demo_file/multi_people_1080p.mp4'
+        args["temp_path"] = './temp_results'
+        args["output_path"] = './output/'
+        args["id_thres"] = 0.03
+        args["no_simswaplogo"] = True
+        args["use_mask"] = True
+        args["crop_size"] = 224
+        args["isTrain"] = False
+
+        return args
